570ec35116173c82d041070bc80fce4fd91a0ea1
Sean Clark <seanc@interactdigitalarts.uk>
2021-08-21 20:59:14 +0100

    Update pollock.py

--- python/pollock.py
+++ python/pollock.py
@@ -1,26 +1,73 @@
+# Pollock Drawing Robot MK2
+# Version 0.0.1
+# Interact Digital Arts
+# 21st August 2021
+# Right motor on pins 6,7,9,10 (display off)
+# Left motor on pins 13,14,15,16
+# Servo on pin 4 (display off)
+# Sensor on pin 8
+# https://www.interactdigitalarts.uk
+
 from microbit import *
 
 pause = 5
 
+display.show(Image.HAPPY)
+sleep(1000)
+
+def forward(steps):
+    display.off()
+    # Left
+    pin13.write_digital(1)
+    pin14.write_digital(0)
+    pin15.write_digital(0)
+    pin16.write_digital(1)
+    # Right
+    pin6.write_digital(1)
+    pin7.write_digital(0)
+    pin8.write_digital(0)
+    pin9.write_digital(1)
+    sleep(pause)
+    # Left
+    pin13.write_digital(0)
+    pin14.write_digital(0)
+    pin15.write_digital(1)
+    pin16.write_digital(1)
+    # Right
+    pin6.write_digital(0)
+    pin7.write_digital(0)
+    pin9.write_digital(1)
+    pin10.write_digital(1)
+    sleep(pause)
+    # Left
+    pin13.write_digital(0)
+    pin14.write_digital(1)
+    pin15.write_digital(1)
+    pin16.write_digital(0)
+    # Right
+    pin6.write_digital(0)
+    pin7.write_digital(1)
+    pin9.write_digital(1)
+    pin10.write_digital(0)
+    sleep(pause)
+    # Left
+    pin13.write_digital(1)
+    pin14.write_digital(1)
+    pin15.write_digital(0)
+    pin16.write_digital(0)
+    # Right
+    pin6.write_digital(1)
+    pin7.write_digital(1)
+    pin9.write_digital(0)
+    pin10.write_digital(0)
+    sleep(pause)
+    display.on()
+
+# def backward(steps):
+# def left(degrees):
+# def right(degrees):
+# def penup:
+# def pendown:
+
 while True:
-    display.show(Image.HAPPY)
-    pin13.write_digital(1)
-    pin14.write_digital(0)
-    pin15.write_digital(0)
-    pin16.write_digital(1)
-    sleep(pause)
-    pin13.write_digital(0)
-    pin14.write_digital(0)
-    pin15.write_digital(1)
-    pin16.write_digital(1)
-    sleep(pause)
-    pin13.write_digital(0)
-    pin14.write_digital(1)
-    pin15.write_digital(1)
-    pin16.write_digital(0)
-    sleep(pause)
-    pin13.write_digital(1)
-    pin14.write_digital(1)
-    pin15.write_digital(0)
-    pin16.write_digital(0)
-    sleep(pause)
+	forward(10)
