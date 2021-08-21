6359732a43f5a3402755e2df4161515c05e6e7e9
Sean Clark <seanc@interactdigitalarts.uk>
2021-08-21 18:55:36 +0100

    Update pollock.py

--- python/pollock.py
+++ python/pollock.py
@@ -1,34 +1,26 @@
-# Pollock Drawing Robot
-# pollock.py v0.0.1
-# Interact Digital Arts
-# 21st August 2021
-
 from microbit import *
 
-motor1 = [13, 14, 15, 16]
 pause = 5
 
-def forward(steps):
-	motor1[0].write_digital(1)
-	motor1[1].write_digital(0)
-	motor1[2].write_digital(0)
-	motor1[3].write_digital(1)
-	sleep(pause)
-	motor1[0].write_digital(0)
-	motor1[1].write_digital(0)
-	motor1[2].write_digital(1)
-	motor1[3].write_digital(1)
-	sleep(pause)
-	motor1[0].write_digital(0)
-	motor1[1].write_digital(1)
-	motor1[2].write_digital(1)
-	motor1[3].write_digital(0)
-	sleep(pause)
-	motor1[0].write_digital(1)
-	motor1[1].write_digital(1)
-	motor1[2].write_digital(0)
-	motor1[3].write_digital(0)
-	sleep(pause)
-
 while True:
-	forward(10)
+    display.show(Image.HAPPY)
+    pin13.write_digital(1)
+    pin14.write_digital(0)
+    pin15.write_digital(0)
+    pin16.write_digital(1)
+    sleep(pause)
+    pin13.write_digital(0)
+    pin14.write_digital(0)
+    pin15.write_digital(1)
+    pin16.write_digital(1)
+    sleep(pause)
+    pin13.write_digital(0)
+    pin14.write_digital(1)
+    pin15.write_digital(1)
+    pin16.write_digital(0)
+    sleep(pause)
+    pin13.write_digital(1)
+    pin14.write_digital(1)
+    pin15.write_digital(0)
+    pin16.write_digital(0)
+    sleep(pause)
