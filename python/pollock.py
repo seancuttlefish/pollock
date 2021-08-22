69eedce6867906ee898b34ba3ab34b1ad0e2a37c
Sean Clark <seanc@interactdigitalarts.uk>
2021-08-22 01:42:16 +0100

    updates

--- python/pollock.py
+++ python/pollock.py
@@ -5,17 +5,16 @@
 # Right motor on pins 6,7,9,10 (display off)
 # Left motor on pins 13,14,15,16
 # Servo on pin 4 (display off)
-# Sensor on pin 8
+# Ultrasonic Sensor on pin 8
 # https://www.interactdigitalarts.uk
 
 from microbit import *
-
-pause = 5
 
 display.show(Image.HAPPY)
 sleep(1000)
 
 def forward(steps):
+    pause = 5
     display.off()
     # Left
     pin13.write_digital(1)
@@ -25,8 +24,8 @@ def forward(steps):
     # Right
     pin6.write_digital(1)
     pin7.write_digital(0)
-    pin8.write_digital(0)
-    pin9.write_digital(1)
+    pin9.write_digital(0)
+    pin10.write_digital(1)
     sleep(pause)
     # Left
     pin13.write_digital(0)
@@ -70,4 +69,4 @@ def forward(steps):
 # def pendown:
 
 while True:
-	forward(10)
+    forward(10)
